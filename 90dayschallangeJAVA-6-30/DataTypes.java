aa4acbb233805dd2090301c6720c7016e22f7ff9
suja-shrestha <shresthasujal917@gmail.com>
2026-06-30 17:32:59 +0545

    Learn about basic datatypes and used then locally

---
+++ 90dayschallangeJAVA-6-30/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        int age = 20;
+        boolean status = true;
+        String name = "sujal";
+        final int mainAge = 20;
+        char initialName = 's';
+
+        
+        System.out.println(age);
+        System.out.println(status);
+        System.out.println(mainAge);
+        System.out.println(initialName);
+        System.out.println(name);
+    }
+
+}
