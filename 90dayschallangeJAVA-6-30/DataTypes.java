ca737b64d922edd614f4c7cce626f394251cbe3e
suja-shrestha <shresthasujal917@gmail.com>
2026-07-01 20:24:37 +0545

    learned + character to add a variable to another variable

--- 90dayschallangeJAVA-6-30/DataTypes.java
+++ 90dayschallangeJAVA-6-30/DataTypes.java
@@ -3,7 +3,9 @@ public class DataTypes {
     public static void main(String[] args) {
         int age = 20;
         boolean status = true;
-        String name = "sujal";
+        String Fname = "sujal ";
+        String Lname = "Shrestha";
+        String Fullname = Fname + Lname;
         final int mainAge = 20;
         char initialName = 's';
 
@@ -12,7 +14,8 @@ public class DataTypes {
         System.out.println(status);
         System.out.println(mainAge);
         System.out.println(initialName);
-        System.out.println(name);
+        System.out.println(Fname + Lname);
+        System.out.println(Fullname);
     }
 
 }
