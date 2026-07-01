bd530712847ca149cbb1ba502ba34972a76a9cfd
suja-shrestha <shresthasujal917@gmail.com>
2026-07-01 20:31:10 +0545

    added float f

--- 90dayschallangeJAVA-6-30/DataTypes.java
+++ 90dayschallangeJAVA-6-30/DataTypes.java
@@ -6,6 +6,7 @@ public class DataTypes {
         String Fname = "sujal ";
         String Lname = "Shrestha";
         String Fullname = Fname + Lname;
+        float num = 22.22f;
         final int mainAge = 20;
         char initialName = 's';
         // One Value to Multiple Variables
@@ -19,6 +20,7 @@ public class DataTypes {
         System.out.println(Fname + Lname);
         System.out.println(Fullname);
         System.out.println(x);
+        System.out.println(num);
 
     }
 
